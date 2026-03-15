58c7da1bebb6bf5d6f2790ed7a5be69c45699901
Enriquenf07 <enriqueferreira956@gmail.com>
2026-03-15 19:10:37 -0300

    running

--- player/player.gd
+++ player/player.gd
@@ -7,18 +7,27 @@ const FRICTION = 2000.0
 @export var ray_length: float = 50.0
 @onready var ray_cast: RayCast2D = %RayCast2D
 var is_still = false
+var is_running = false
+
+func get_speed():
+	return SPEED if !is_running else SPEED * 1.5
 
 func _physics_process(delta: float) -> void:
 	if(is_still): return
 	var direction := Input.get_vector('left', 'right', 'up', 'down').normalized()
 	if direction != Vector2.ZERO:
 		ray_cast.target_position = direction * ray_length
-		velocity = direction * SPEED
+		velocity = direction * get_speed()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide()
 	
 func _unhandled_input(event: InputEvent) -> void:
+	if(event.is_action_pressed("run")):
+		is_running = true
+		return
+	if(event.is_action_released("run")):
+		is_running = false
 	if(event.is_action_pressed('interact') and ray_cast.is_colliding()):
 		var collider := ray_cast.get_collider()
 		if(collider is InteractableObject):
