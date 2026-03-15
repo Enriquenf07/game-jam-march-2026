4d21f5e16ee5872b49cb61ce3f4ab4d70c93053a
Enriquenf07 <enriqueferreira956@gmail.com>
2026-03-15 16:23:25 -0300

    player interactions and merge

--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
-
+class_name Player
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const FRICTION = 2000.0
-
+@export var ray_length: float = 50.0
+@onready var ray_cast: RayCast2D = %RayCast2D
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector('left', 'right', 'up', 'down')
-	if direction:
+	var direction := Input.get_vector('left', 'right', 'up', 'down').normalized()
+	if direction != Vector2.ZERO:
+		ray_cast.target_position = direction * ray_length
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
---
+++ world/world.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@export var lerp_speed: float = 5.
+@onready var camera := %Camera
+var player: Player
+
+func _ready() -> void:
+	var factory := PlayerFactory.new()
+	player = factory.create(Vector2(0, 0), self)
+	pass 
+
+func _physics_process(delta: float) -> void:
+	if(player != null):
+		var target_pos = player.global_position
+		camera.global_position = camera.global_position.lerp(target_pos, lerp_speed * delta)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
