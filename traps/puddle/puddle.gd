612aa8462f4b8c389806515bd04852b444b16c6f
ClaytonWheeler0205 <Clayton02.Wheeler05@gmail.com>
2026-03-22 13:19:26 -0500

    Added assets and sfx for slipping
    Added player animations for slipping and falling, and sfx for slipping on the puddle/peel and stepping on the puddle.

--- traps/puddle/puddle.gd
+++ traps/puddle/puddle.gd
@@ -1,11 +1,15 @@
 class_name Puddle
 extends ContinuousNoiseTrap
 
+@export var _puddle_step_sounds: Array[AudioStream]
 @export var _slipping_sound_player: AudioStreamPlayer2D
 @export var _slipping_sound_collision: CollisionShape2D
 
+func _ready() -> void:
+	super._ready()
+	_trap_trigger_sound.stream = _puddle_step_sounds[randi_range(0, _puddle_step_sounds.size() - 1)]
+
 func _process(_delta: float) -> void:
-	# TODO: call the player method "slip()" if the player is running
 	if (_player_in_trap != null and _is_running()):
 		_player_in_trap.slip_on_puddle()
 		_slipping_sound_player.play()
@@ -19,12 +23,15 @@ func _is_running() -> bool:
 func _disarm_decorator():
 	print("puddle disarmed")
 
+func _on_trap_activation_sound_finished() -> void:
+	super._on_trap_activation_sound_finished()
+	_trap_trigger_sound.stream = _puddle_step_sounds[randi_range(0, _puddle_step_sounds.size() - 1)]
+
 func _on_trap_disarmed():
 	_trap_visual.hide()
 
 func _on_slipping_sound_player_finished() -> void:
 	_slipping_sound_collision.set_deferred("disabled", true)
-
 
 func _on_disarm_sound_player_finished() -> void:
 	queue_free()
