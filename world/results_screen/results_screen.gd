dcceb988d7fbaa46f5f09ecd50e335d9fd193152
ClaytonWheeler0205 <Clayton02.Wheeler05@gmail.com>
2026-03-22 14:46:28 -0500

    Updated win and lose screens

--- world/results_screen/results_screen.gd
+++ world/results_screen/results_screen.gd
@@ -4,18 +4,19 @@ extends CanvasLayer
 signal game_exited
 
 @export var _win_screen: Control
+@export var _win_button: Button
 @export var _score_text: Label
 @export var _lose_screen: Control
+@export var _lose_button: Button
 
 func show_win_screen(total_score: int):
 	_win_screen.show()
-	_score_text.text = "Total Stolen: $" + str(total_score)
+	_win_button.grab_focus()
+	_score_text.text = "Total Stolen:\n$" + str(total_score)
 
 func _on_exit_button_pressed() -> void:
 	game_exited.emit()
 
 func show_lose_screen():
 	_lose_screen.show()
-
-func _on_retry_button_pressed() -> void:
-	get_tree().reload_current_scene()
+	_lose_button.grab_focus()
--- world/title_screen/title_screen.gd
+++ world/title_screen/title_screen.gd
@@ -3,6 +3,10 @@ extends Control
 
 signal game_started
 
+@export var _start_button: Button
+
+func _ready() -> void:
+	_start_button.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	game_started.emit()
